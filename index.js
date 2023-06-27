c0e68173f3c8a014034ff13ef525193d49b90e3e
Tyler Sanders <tsanders5902@gmail.com>
2023-06-26 20:14:29 -0400

    preliminary forming of some of the layout, menu buttons

--- index.js
+++ index.js
@@ -8,3 +8,29 @@ function getValue() {
     alert(name.value + number.value + email.value + date.value + guests.value + notes.value);
     
 }
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("modalBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if(event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function displaySection(section) {
+    document.getElementById('canapesSection').style.display = 'none';
+    document.getElementById('dessertSection').style.display = 'none';
+    document.getElementById('charcuterieSection').style.display = 'none';
+
+    document.getElementById(section + 'Section').style.display = 'block';
+}
